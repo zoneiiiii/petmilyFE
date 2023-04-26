cf39607b91cc189db0edcd44b32fddd87960631f
Taemin Eom <umtaemin1125@naver.com>
2023-04-26 15:48:40 +0900

    feat:라우터 수정, 로딩페이지 추가, Path 수정

--- src/pages/ImportPages.js
+++ src/pages/ImportPages.js
@@ -6,7 +6,8 @@ const Login = lazy(() => import("./Login/Login"));
 const MyPage = lazy(() => import ("./Mypage/MyPageInfo")); //마이페이지 들어갔을때 기본적으로 회원정보 표시하기 위함.
 const MyPageInfo = lazy(() => import ("./Mypage/MyPageInfo"));
 const ModifyInfo = lazy(() => import ("./Mypage/ModifyInfo"));
+const Donate = lazy(() => import ("./Support/Donate"));
 //아래에 import할 페이지 함수 생성
 
 
-export {Main, Login, MyPage,MyPageInfo,ModifyInfo};
+export {Main, Login, MyPage,MyPageInfo,ModifyInfo, Donate};
