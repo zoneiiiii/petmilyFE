1a4f1d259e43a5d94f7eb42101b8c66037e332ac
hyeonny <k.d.hyeon97@gmail.com>
2023-04-26 15:48:31 +0900

    fix: 마이페이지 링크 임시로 제작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BROWSER_PATH } from "../../constants/path";
+import { Link } from "react-router-dom";
 
 function Main() {
-  
-
   return (
     <div>
-    <h1>메인페이지 작성</h1>
+      <h1>메인페이지 작성</h1>
+      <Link to={BROWSER_PATH.MYPAGE}>마이페이지</Link>
     </div>
-  )
+  );
 }
 
 export default Main;
