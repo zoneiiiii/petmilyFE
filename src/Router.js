37c421bfc7b437536d415370152249028284f0de
hyeonny <k.d.hyeon97@gmail.com>
2023-04-26 15:45:21 +0900

    fix: Router.js suspense추가, MyPageInfo경로 수정(mypageinfo->mypage), mypage관련페이지 중첩라우터

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { BROWSER_PATH } from './constants/path';
-import {Main, Login, MyPageInfo, ModifyInfo} from './pages/ImportPages';
-
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BROWSER_PATH } from "./constants/path";
+import {
+  Main,
+  Login,
+  MyPageInfo,
+  ModifyInfo,
+  MyPage,
+} from "./pages/ImportPages";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={BROWSER_PATH.MAIN} element={<Main />} />
-        <Route path={BROWSER_PATH.LOGIN} element={<Login />} />
-        <Route path={BROWSER_PATH.MYPAGE} element={<MyPageInfo/>}/> 
-        <Route path={BROWSER_PATH.MYPAGEINFO} element={<MyPageInfo/>}/>
-        <Route path={BROWSER_PATH.MODIFYINFO} element={<ModifyInfo/>}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={BROWSER_PATH.MAIN} element={<Main />} />
+          <Route path={BROWSER_PATH.LOGIN} element={<Login />} />
+          <Route exact path={BROWSER_PATH.MYPAGE} element={<MyPage />}>
+            <Route index element={<MyPageInfo />} />
+            <Route path={BROWSER_PATH.MODIFYINFO} element={<ModifyInfo />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
--- src/pages/ImportPages.js
+++ src/pages/ImportPages.js
@@ -1,12 +1,10 @@
 import { lazy } from "react";
 
-
 const Main = lazy(() => import("./Main/Main"));
 const Login = lazy(() => import("./Login/Login"));
-const MyPage = lazy(() => import ("./Mypage/MyPageInfo")); //마이페이지 들어갔을때 기본적으로 회원정보 표시하기 위함.
-const MyPageInfo = lazy(() => import ("./Mypage/MyPageInfo"));
-const ModifyInfo = lazy(() => import ("./Mypage/ModifyInfo"));
+const MyPage = lazy(() => import("./Mypage/MyPage")); //마이페이지 들어갔을때 기본적으로 회원정보 표시하기 위함.
+const MyPageInfo = lazy(() => import("./Mypage/MyPageInfo"));
+const ModifyInfo = lazy(() => import("./Mypage/ModifyInfo"));
 //아래에 import할 페이지 함수 생성
 
-
-export {Main, Login, MyPage,MyPageInfo,ModifyInfo};
+export { Main, Login, MyPage, MyPageInfo, ModifyInfo };
