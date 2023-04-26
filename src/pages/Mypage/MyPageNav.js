cb0502e6e1d964ebcaeb5156aef9262c6ac42e58
hyeonny <k.d.hyeon97@gmail.com>
2023-04-26 15:47:18 +0900

    fix: 각link마다 경로와 이름 설정

--- src/pages/Mypage/MyPageNav.js
+++ src/pages/Mypage/MyPageNav.js
@@ -1,21 +1,35 @@
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { BROWSER_PATH } from "../../constants/path";
+
 const ProfileSrc = "/images/emptyProfile.png";
 const MyPageNav = () => {
   return (
     <MyPageNavStyle className="MyPageNav">
       <img className="ProfileImg" src={ProfileSrc} alt="profile" />
-      <NavList title={"My Page"} navList={["회원 정보", "구매 내역"]} />
-      <NavList title={"입양 관리"} navList={["입양 후기", "입양 내역"]} />
+      <NavList
+        title={"My Page"}
+        navList={[
+          { linkName: "회원 정보", link: BROWSER_PATH.MYPAGE },
+          { linkName: "구매 내역", link: BROWSER_PATH.MYPAGEORDER }, // 페이지 없음
+        ]}
+      />
+      <NavList
+        title={"입양 관리"}
+        navList={[
+          { linkName: "입양 후기", links: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+          { linkName: "입양 내역", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+        ]}
+      />
       <NavList
         title={"쓴 글 목록"}
         navList={[
-          "실종 동물 게시판",
-          "목격 제보 게시판",
-          "자유게시판",
-          "봉사 후기",
-          "매매 장터",
+          { linkName: "실종 동물 게시판", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+          { linkName: "목격 제보 게시판", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+          { linkName: "자유게시판", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+          { linkName: "봉사 후기", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
+          { linkName: "매매 장터", link: BROWSER_PATH.MYPAGE }, // 링크없어서 마이페이지로 연결, 페이지 없음
         ]}
       />
     </MyPageNavStyle>
@@ -41,10 +55,10 @@ const NavList = ({ title, navList, isSelected }) => {
     >
       <div className="NavTitle">{title}</div>
       <ul ref={ulRef}>
-        {navList.map((link, index) => {
+        {navList.map((links, index) => {
           return (
             <li key={index}>
-              <Link to="">{link}</Link>
+              <Link to={links.link}>{links.linkName}</Link>
             </li>
           );
         })}
