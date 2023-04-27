e6c91d666c84de2fd757530ee9469b5a3b39b2d5
sam4877 <8ree_@naver.com>
2023-04-27 16:50:47 +0900

    no message

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { BROWSER_PATH } from './constants/path';
-import {Main, Login, Join, MyPageInfo, ModifyInfo} from './pages/ImportPages';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BROWSER_PATH } from "./constants/path";
+import {
+  Main,
+  Join,
+  Login,
+  MyPageInfo,
+  ModifyInfo,
+  MyPage,
+  Donate,
+  FindPW,
+  ChangePW,
+} from "./pages/ImportPages";
+import NotFound from "./pages/NotFound/NotFound";
+import Loading from "./components/Loading/LoadingPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={BROWSER_PATH.MAIN} element={<Main />} />
-        <Route path={BROWSER_PATH.LOGIN} element={<Login />} />
-        <Route path={BROWSER_PATH.JOIN} element={<Join />} />
-      </Routes>
-
-      <Routes>
-        <Route path={BROWSER_PATH.MYPAGE} element={<MyPageInfo/>}>
-          <Route path={BROWSER_PATH.MYPAGEINFO} element={<MyPageInfo/>}/>
-          <Route path={BROWSER_PATH.MODIFYINFO.toLowerCase()} element={<ModifyInfo/>}/>
-        </Route>  
-      </Routes>
+      <React.Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path={BROWSER_PATH.MAIN} element={<Main />} />
+          <Route path={BROWSER_PATH.JOIN} element={<Join />} />
+          <Route path={BROWSER_PATH.LOGIN} element={<Login />} />
+          <Route exact path={BROWSER_PATH.MYPAGE} element={<MyPage />}>
+            <Route index element={<MyPageInfo />} />
+            <Route path={BROWSER_PATH.MODIFYINFO} element={<ModifyInfo />} />
+          </Route>
+          <Route path={BROWSER_PATH.SUPPORT} element={<Donate />} />
+          <Route path={BROWSER_PATH.FINDPW} element={<FindPW />} />
+          <Route path={BROWSER_PATH.CHANGEPW} element={<ChangePW />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </React.Suspense>
     </BrowserRouter>
   );
 };
--- src/pages/ImportPages.js
+++ src/pages/ImportPages.js
@@ -1,13 +1,25 @@
 import { lazy } from "react";
 
-
 const Main = lazy(() => import("./Main/Main"));
-const Login = lazy(() => import("./Login/Login"));
 const Join = lazy(() => import("./Join/Join"));
-const MyPage = lazy(() => import ("./Mypage/MyPageInfo")); //마이페이지 들어갔을때 기본적으로 회원정보 표시하기 위함.
-const MyPageInfo = lazy(() => import ("./Mypage/MyPageInfo"));
-const ModifyInfo = lazy(() => import ("./Mypage/ModifyInfo"));
+const Login = lazy(() => import("./Login/Login"));
+const MyPage = lazy(() => import("./Mypage/MyPage")); //마이페이지 들어갔을때 기본적으로 회원정보 표시하기 위함.
+const MyPageInfo = lazy(() => import("./Mypage/MyPageInfo"));
+const ModifyInfo = lazy(() => import("./Mypage/ModifyInfo"));
+const Donate = lazy(() => import("./Support/Donate"));
+const FindPW = lazy(() => import("./Login/FindPW"));
+const ChangePW = lazy(() => import("./Login/ChangePW"));
+
 //아래에 import할 페이지 함수 생성
 
-
-export {Main, Login, Join, MyPage,MyPageInfo,ModifyInfo};
+export {
+  Main,
+  Join,
+  Login,
+  MyPage,
+  MyPageInfo,
+  ModifyInfo,
+  Donate,
+  FindPW,
+  ChangePW,
+};
