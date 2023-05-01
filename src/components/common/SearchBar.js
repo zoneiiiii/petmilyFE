33460a79f19211cebc0a229337431aa89484ff90
hyeonny <k.d.hyeon97@gmail.com>
2023-05-02 01:48:25 +0900

    FEAT:(mui)커스텀searchbar,datepicker,theme생성

---
+++ src/components/common/SearchBar.js
@@ -0,0 +1,54 @@
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
+import { CustomTheme } from "../../assets/Theme/CustomTheme";
+
+export const SearchBar = () => {
+  return (
+    <ThemeProvider theme={CustomTheme}>
+      <Box>
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="Search..."
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon color="fbd385" />
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            mr: 2,
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                // 평상시의 테두리
+                borderColor: "#fbd385",
+              },
+              "&:hover fieldset": {
+                // 마우스 올린 상태의 테두리
+                borderColor: "#1976d2",
+              },
+              // "&.Mui-focused fieldset": { // 클릭된 상태의 테두리
+              //   borderColor: "#fbd385",
+              // },
+            },
+          }}
+        />
+        <Button
+          variant="contained"
+          theme={CustomTheme}
+          color="fbd385"
+          style={{ marginLeft: "10px" }}
+        >
+          검색
+        </Button>
+      </Box>
+    </ThemeProvider>
+  );
+};
