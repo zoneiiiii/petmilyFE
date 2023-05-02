e28837ed748cf56a51aff7f2f28c78676040b7c7
hyeonny <k.d.hyeon97@gmail.com>
2023-05-02 22:38:15 +0900

    FIX: 경로 설정 변경( 충돌주의!)

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import * as Page from "./pages/ImportPages";
 import * as BROWSER_PATH from "./constants/PageURL";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <React.Suspense fallback={<Page.Loading />}>
         <Routes>
           <Route path={BROWSER_PATH.MAIN} element={<Page.Layout />}>
@@ -26,6 +27,7 @@ const Router = () => {
               path={BROWSER_PATH.ACCOUNT.CHANGE_PW}
               element={<Page.Account.ChangePW />}
             />
+            {/* MyPage */}
             <Route
               path={BROWSER_PATH.MYPAGE.MODIFY_INFO}
               element={<Page.MyPage.ModifyInfo />}
@@ -49,25 +51,34 @@ const Router = () => {
                 element={<Page.MyPage.MyPageAdoptReview />}
               />
               <Route
+                path={BROWSER_PATH.MYPAGE.ADOPTLIST}
+                element={<Page.MyPage.MyPageAdoptList />}
+              />
+              <Route
                 path={BROWSER_PATH.MYPAGE.BOARD()}
                 element={<Page.MyPage.MyPageBoard />}
               />
               <Route
                 path={BROWSER_PATH.MYPAGE.QNA}
-                element={<Page.MyPage.MyPageInquiry />}
+                element={<Page.MyPage.MyPageQnA />}
               />
               <Route
                 path={BROWSER_PATH.MYPAGE.QNA_WRITE}
-                element={<Page.MyPage.MyPageQnA />}
+                element={<Page.MyPage.MyPageQnAWrite />}
               />
               <Route
                 path={BROWSER_PATH.MYPAGE.QNA_DETAIL}
                 element={<Page.MyPage.MyPageQnADetail />}
               />
             </Route>
+            {/* About */}
             <Route
               path={BROWSER_PATH.ABOUT.ABOUT}
               elememt={<Page.About.About />}
+            />
+            <Route
+              path={BROWSER_PATH.ABOUT.ADOPT_PROCESS}
+              elememt={<Page.About.AdoptProcess />}
             />
             <Route
               path={BROWSER_PATH.ABOUT.ACTIVITY}
@@ -76,18 +87,6 @@ const Router = () => {
             <Route
               path={BROWSER_PATH.ABOUT.ACTIVITY_DETAIL()}
               elememt={<Page.About.ActivityDetail />}
-            />
-            <Route
-              path={BROWSER_PATH.ABOUT.ADOPT_PROCESS}
-              elememt={<Page.About.AdoptProcess />}
-            />
-            <Route
-              path={BROWSER_PATH.ABOUT.ABOUT}
-              elememt={<Page.About.ApplicationForm />}
-            />
-            <Route
-              path={BROWSER_PATH.ABOUT.ABOUT}
-              elememt={<Page.About.About />}
             />
             <Route path={BROWSER_PATH.ABOUT.FAQ} elememt={<Page.About.FAQ />} />
             <Route
@@ -102,10 +101,62 @@ const Router = () => {
               path={BROWSER_PATH.ABOUT.NOTICE_WRITE}
               elememt={<Page.About.NoticeWrite />}
             />
+            {/* Adopt */}
+            <Route
+              path={BROWSER_PATH.ADOPT.CHECKLIST}
+              elememt={<Page.Adopt.AdoptChecklist />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.ANIMAL_LIST}
+              elememt={<Page.Adopt.AnimalList />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.ANIMAL_LIST_DETAIL}
+              elememt={<Page.Adopt.AnimalListDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.APPLICATION}
+              elememt={<Page.Adopt.AdoptApplication />}
+            />
             <Route
               path={BROWSER_PATH.ADOPT.HOSPITAL_LOCATION}
               element={<Page.Adopt.HospitalLocation />}
             />
+            <Route
+              path={BROWSER_PATH.ADOPT.SHELTER_LOCATION}
+              element={<Page.Adopt.ShelterLocation />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.ADOPT_REVIEW}
+              elememt={<Page.Adopt.AdoptReview />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.ADOPT_REVIEW_DETAIL}
+              elememt={<Page.Adopt.AdoptReviewDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.ADOPT.ADOPT_REVIEW_WRITE}
+              elememt={<Page.Adopt.AdoptReviewWrite />}
+            />
+            {/* Shop */}
+            <Route
+              path={BROWSER_PATH.SHOP.PRODUCT}
+              element={<Page.Shop.Product />}
+            />
+            <Route
+              path={BROWSER_PATH.SHOP.PRODUCT_DETAIL}
+              element={<Page.Shop.ProductDetail />}
+            />
+            <Route path={BROWSER_PATH.SHOP.CART} element={<Page.Shop.Cart />} />
+            <Route
+              path={BROWSER_PATH.SHOP.ORDER}
+              element={<Page.Shop.Order />}
+            />
+            <Route
+              path={BROWSER_PATH.SHOP.ORDER_COMPLETE}
+              element={<Page.Shop.OrderComplete />}
+            />
+            {/* Support */}
             <Route
               path={BROWSER_PATH.SUPPORT.DONATE}
               element={<Page.Support.Donate />}
@@ -115,21 +166,112 @@ const Router = () => {
               element={<Page.Support.DonateApply />}
             />
             <Route
-              path={BROWSER_PATH.SUPPORT.VOLUNTEER}
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_NOTICE}
               element={<Page.Support.VolunteerNotice />}
             />
             <Route
-              path={BROWSER_PATH.SUPPORT.VOLUNTEER_DETAIL}
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_NOTICE_DETAIL}
               element={<Page.Support.VolunteerNoticeDetail />}
             />
             <Route
-              path={BROWSER_PATH.SUPPORT.VOLUNTEER_WRITE}
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_NOTICE_WRITE}
               element={<Page.Support.VolunteerNoticeWrite />}
             />
             <Route
-              path={BROWSER_PATH.SHOP.PRODUCT}
-              element={<Page.Shop.Product />}
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_REVIEW}
+              element={<Page.Support.VolunteerReview />}
             />
+            <Route
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_REVIEW_DETAIL}
+              element={<Page.Support.VolunteerReviewDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.SUPPORT.VOLUNTEER_REVIEW_WRITE}
+              element={<Page.Support.VolunteerReviewWrite />}
+            />
+            {/* Community */}
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FIND}
+              element={<Page.Community.FindBoard />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FIND_DETAIL}
+              element={<Page.Community.FindBoardDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FIND_WRITE}
+              element={<Page.Community.FindBoardWrite />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FLEA}
+              element={<Page.Community.FleaBoard />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FLEA_DETAIL}
+              element={<Page.Community.FleaBoardDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FLEA_WRITE}
+              element={<Page.Community.FleaBoardWrite />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FREE}
+              element={<Page.Community.FreeBoard />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FREE_DETAIL}
+              element={<Page.Community.FreeBoardDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.FREE_WRITE}
+              element={<Page.Community.FreeBoardWrite />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.MISSING}
+              element={<Page.Community.Missing />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.MISSING_DETAIL}
+              element={<Page.Community.MissingDetail />}
+            />
+            <Route
+              path={BROWSER_PATH.COMMUNITY.MISSING_WRITE}
+              element={<Page.Community.MissingWrite />}
+            />
+            {/* Admin */}
+            <Route
+              path={BROWSER_PATH.ADMIN.ADOPT}
+              element={<Page.Admin.AdminAdopt />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.BOARD}
+              element={<Page.Admin.AdminBoard />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.DASHBOARD}
+              element={<Page.Admin.AdminDashBoard />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.MEMBER}
+              element={<Page.Admin.AdminMember />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.ORDER}
+              element={<Page.Admin.AdminOrder />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.PRODUCT}
+              element={<Page.Admin.AdminProduct />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.PRODUCTFORM}
+              element={<Page.Admin.AdminProductWrite />}
+            />
+            <Route
+              path={BROWSER_PATH.ADMIN.QNA}
+              element={<Page.Admin.AdminQnA />}
+            />
+
             <Route path="*" element={<Page.NotFound />} />
           </Route>
         </Routes>
@@ -137,5 +279,15 @@ const Router = () => {
     </BrowserRouter>
   );
 };
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default Router;
--- src/constants/PageURL.js
+++ src/constants/PageURL.js
@@ -3,269 +3,309 @@ export const MAIN = "/";
 
 /**계정 관련 URL 리스트 */
 export const ACCOUNT = {
-	/**로그인 페이지 URL
-	 * @return "/login" */
-	LOGIN: "/login",
-	/**회원가입 페이지 URL
-	 * @return "/join" */
-	JOIN: "/join",
-	/**비밀번호 찾기 페이지 URL
-	 * @return "/findpw" */
-	FIND_PW: "/findpw",
-	/**비밀번호 변경 페이지 URL
-	 * @return "/changepw" */
-	CHANGE_PW: "/changepw",
+  /**로그인 페이지 URL
+   * @return "/login" */
+  LOGIN: "/login",
+  /**회원가입 페이지 URL
+   * @return "/join" */
+  JOIN: "/join",
+  /**비밀번호 찾기 페이지 URL
+   * @return "/findpw" */
+  FIND_PW: "/findpw",
+  /**비밀번호 변경 페이지 URL
+   * @return "/changepw" */
+  CHANGE_PW: "/changepw",
 };
 
 /** MYPAGE: 마이페이지 관련 URL 리스트 */
 export const MYPAGE = {
-	/**마이페이지 정보 페이지 URL
-	 * @return "/mypage"
-	 */
-	INFO: "/mypage",
-	/**회원 정보 수정 페이지 URL
-	 * @return "/mypage/modifyinfo"
-	 */
-	MODIFY_INFO: "/mypage/modifyinfo",
-	/**구매 내역 페이지 URL
-	 * @return "/mypage/orderlist"
-	 */
-	ORDERLIST: "/mypage/orderlist",
-	/**
-	 ** 활동내역 상세페이지 URL
-	 ** 사용법: ABOUT.ACTIVITY_DETAIL(boardId)
-	 * @param {string} boardId 게시판id
-	 * @return "/mypage/order/:orderNum" (orderNum없음)
-	 * @return "/mypage/order/" + orderNum (orderNum존재)
-	 **/
-	ORDER_DETAIL: (orderNum) => {
-		if (orderNum === undefined) return "/mypage/order/list?orderNum=:orderNum";
-		else return "/mypage/order/list?orderNum=" + orderNum;
-	},
-	/**입양 후기 페이지 URL
-	 * @return "/mypage/adoptreview"
-	 */
-	ADOPT_REVIEW: "/mypage/adoptreview",
-	/**입양 내역 페이지 URL
-	 * @return "/mypage/adopt"
-	 */
-	ADOPT: "/mypage/adopt",
-	/**
-	 ** 내가 쓴 글 페이지 URL
-	 ** 사용법: MYPAGE.BOARD(boardId)
-	 * @param {string} boardId 게시판id
-	 * @return "/mypage/board/:boardId" (boardId없음)
-	 * @return "/mypage/board/" + boardId (boardId존재)
-	 **/
-	BOARD: (boardId) => {
-		if (boardId === undefined) return "/mypage/board/:boardId";
-		else return "/mypage/board/" + boardId;
-	},
-	/**1:1 문의 페이지 URL
-	 * @return "/mypage/qna"
-	 */
-	QNA: "/mypage/qna",
-	/**1:1 문의 세부 페이지 URL
-	 * @return "/mypage/qna"
-	 */
-	QNA_DETAIL: "/mypage/qna",
-	/**1:1 문의 작성 페이지 URL
-	 * @return "/mypage/qna/write"
-	 */
-	QNA_WRITE: "/mypage/qna/write",
+  /**마이페이지 정보 페이지 URL
+   * @return "/mypage"
+   */
+  INFO: "/mypage",
+  /**회원 정보 수정 페이지 URL
+   * @return "/mypage/modifyinfo"
+   */
+  MODIFY_INFO: "/mypage/modifyinfo",
+  /**구매 내역 페이지 URL
+   * @return "/mypage/orderlist"
+   */
+  ORDERLIST: "/mypage/orderlist",
+  /**
+   ** 활동내역 상세페이지 URL
+   ** 사용법: MYPAGE.ORDER_DETAIL(orderNum)
+   * @param  orderNum 게시판id
+   * @return "/mypage/order/:orderNum" (orderNum없음)
+   * @return "/mypage/order/" + orderNum (orderNum존재)
+   **/
+  ORDER_DETAIL: (orderNum) => {
+    if (orderNum === undefined) return "/mypage/order/list?orderNum=:orderNum";
+    else return "/mypage/order/list?orderNum=" + orderNum;
+  },
+  /**입양 후기 페이지 URL
+   * @return "/mypage/adoptreview"
+   */
+  ADOPT_REVIEW: "/mypage/adoptreview",
+  /**입양 내역 페이지 URL
+   * @return "/mypage/adopt"
+   */
+  ADOPTLIST: "/mypage/adoptlist",
+  /**
+   ** 내가 쓴 글 페이지 URL
+   ** 사용법: MYPAGE.BOARD(boardId)
+   * @param  boardId 게시판id
+   * @return "/mypage/board/:boardId" (boardId없음)
+   * @return "/mypage/board/" + boardId (boardId존재)
+   **/
+  BOARD: (boardId) => {
+    if (boardId === undefined) return "/mypage/board/:boardId";
+    else return "/mypage/board/" + boardId;
+  },
+  /**1:1 문의 페이지 URL
+   * @return "/mypage/qna"
+   */
+  QNA: "/mypage/qna",
+  /**1:1 문의 세부 페이지 URL
+   * @return "/mypage/qna/:id"
+   */
+  QNA_DETAIL: "/mypage/qna/:id",
+  /**1:1 문의 작성 페이지 URL
+   * @return "/mypage/qna/write"
+   */
+  QNA_WRITE: "/mypage/qna/write",
 };
 
 /** ABOUT: 소개 관련 URL 리스트*/
 export const ABOUT = {
-	/**프로젝트 소개 페이지 URL
-	 * @return "/about" */
-	ABOUT: "/about",
-	/**입양 절차 안내 페이지 URL
-	 * @return "/about/adoptprocess" */
-	ADOPT_PROCESS: "/about/adoptprocess",
-	/**공지사항 페이지 URL
-	 * @return "/notice" */
-	NOTICE: "/notice",
-	/**
-	 ** 공지사항 상세 페이지 URL
-	 ** 사용법: ABOUT.NOTICE_DETAIL(boardId)
-	 * @param {string} boardId 게시판id
-	 * @return "/notice/:boardId" (boardId없음)
-	 * @return "/notice/" + boardNum (boardId존재)
-	 **/
-	NOTICE_DETAIL: (boardNum) => {
-		if (boardNum === undefined) return "/notice/:boardNum";
-		else return "/notice/" + boardNum;
-	},
-	/**공지사항 작성 페이지 URL
-	 * @return "/notice/write"
-	 */
-	NOTICE_WRITE: "/notice/write",
-	/**활동내역 페이지 URL
-	 * @return "/notice/write"
-	 */
-	ACTIVITY: "/activity",
-	/**
-	 ** 활동내역 상세페이지 URL
-	 ** 사용법: ABOUT.ACTIVITY_DETAIL(boardId)
-	 * @param {string} boardId 게시판id
-	 * @return "/activity/:boardId" (boardId없음)
-	 * @return "/activity/" + BoardId (boardId존재)
-	 **/
-	ACTIVITY_DETAIL: (BoardId) => {
-		if (BoardId === undefined) return "/activity/:BoardId";
-		else return "/activity/" + BoardId;
-	},
-	/**FAQ페이지 URL
-	 * @return "/faq"
-	 */
-	FAQ: "/faq", // FAQ
+  /**프로젝트 소개 페이지 URL
+   * @return "/about" */
+  ABOUT: "/about",
+  /**입양 절차 안내 페이지 URL
+   * @return "/about/adoptprocess" */
+  ADOPT_PROCESS: "/about/adoptprocess",
+  /**공지사항 페이지 URL
+   * @return "/notice" */
+  NOTICE: "/notice",
+  /**
+   ** 공지사항 상세 페이지 URL
+   ** 사용법: ABOUT.NOTICE_DETAIL(boardId)
+   * @param {string} boardId 게시판id
+   * @return "/notice/:boardId" (boardId없음)
+   * @return "/notice/" + boardNum (boardId존재)
+   **/
+  NOTICE_DETAIL: (boardNum) => {
+    if (boardNum === undefined) return "/notice/:boardNum";
+    else return "/notice/" + boardNum;
+  },
+  /**공지사항 작성 페이지 URL
+   * @return "/notice/write"
+   */
+  NOTICE_WRITE: "/notice/write",
+  /**활동내역 페이지 URL
+   * @return "/notice/write"
+   */
+  ACTIVITY: "/activity",
+  /**
+   ** 활동내역 상세페이지 URL
+   ** 사용법: ABOUT.ACTIVITY_DETAIL(boardId)
+   * @param {string} boardId 게시판id
+   * @return "/activity/:boardId" (boardId없음)
+   * @return "/activity/" + BoardId (boardId존재)
+   **/
+  ACTIVITY_DETAIL: (BoardId) => {
+    if (BoardId === undefined) return "/activity/:BoardId";
+    else return "/activity/" + BoardId;
+  },
+  /**FAQ페이지 URL
+   * @return "/faq"
+   */
+  FAQ: "/faq", // FAQ
 };
 
 /** ADOPT: 입양 관련 URL 리스트*/
 export const ADOPT = {
-	/**입양 후기 게시판 URL
-	 * @return "/adopt/review"
-	 */
-	REVIEW: "/adopt/review",
-	/**입양 후기 상세 페이지 URL
-	 * @return "adopt/review/detail"
-	 */
-	REVIEW_DETAIL: "adopt/review/detail",
-	/**입양 후기 작성 페이지 URL
-	 * @return "/adopt/review/write"
-	 */
-	REVIEW_WRITE: "/adopt/review/write",
-	/**입양 신청 페이지 URL
-	 * @return "/adopt/application"
-	 */
-	APPLICATION: "/adopt/application",
-	/**입양 체크리스트 페이지 URL
-	 * @return "/adopt/checklist"
-	 */
-	CHECKLIST: "/adopt/checklist", // 입양 체크리스트
-	/**동물병원 위치 페이지 URL
-	 * @return "/location/hospital"
-	 */
-	HOSPITAL_LOCATION: "/location/hospital", // 동물 병원 위치
-	/**보호소 위치 페이지 URL
-	 * @return "/location/shelter"
-	 */
-	SHELTER_LOCATION: "/location/shelter", // 보호소 위치
-	/**유기동물 리스트 페이지 URL
-	 * @return "/adopt/animal"
-	 */
-	ANIMAL_LIST: "/adopt/animal", // 유기동물 리스트 + "/:ID": 유기동물 상세
+  /**입양 후기 게시판 URL
+   * @return "/adopt/review"
+   */
+  ADOPT_REVIEW: "/adopt/review",
+  /**입양 후기 상세 페이지 URL
+   * @return "adopt/review/detail"
+   */
+  ADOPT_REVIEW_DETAIL: "adopt/review/detail",
+  /**입양 후기 작성 페이지 URL
+   * @return "/adopt/review/write"
+   */
+  ADOPT_REVIEW_WRITE: "/adopt/review/write",
+  /**입양 신청 페이지 URL
+   * @return "/adopt/application"
+   */
+  APPLICATION: "/adopt/application",
+  /**입양 체크리스트 페이지 URL
+   * @return "/adopt/checklist"
+   */
+  CHECKLIST: "/adopt/checklist", // 입양 체크리스트
+  /**동물병원 위치 페이지 URL
+   * @return "/location/hospital"
+   */
+  HOSPITAL_LOCATION: "/location/hospital", // 동물 병원 위치
+  /**보호소 위치 페이지 URL
+   * @return "/location/shelter"
+   */
+  SHELTER_LOCATION: "/location/shelter", // 보호소 위치
+  /**유기동물 리스트 페이지 URL
+   * @return "/adopt/animal"
+   */
+  ANIMAL_LIST: "/adopt/animal", // 유기동물 리스트 + "/:ID": 유기동물 상세
+  /**유기동물 리스트 상세 페이지 URL
+   * @return "/adopt/animal/:id"
+   */
+  ANIMAL_LIST_DETAIL: "/adopt/animal/:id",
 };
 
 /** SHOP: SHOP관련 URL 리스트*/
 export const SHOP = {
-	/**상품리스트 페이지 URL
-	 * @return "/product"
-	 */
-	PRODUCT: "/product",
-	/**카트 페이지 URL
-	 * @return "/cart"
-	 */
-	CART: "/cart",
-	/**주문 페이지 URL
-	 * @return "/order"
-	 */
-	ORDER: "/order",
-	/**주문 완료 페이지 URL
-	 * @return "/order/complete"
-	 */
-	ORDER_COMPLETE: "/order/complete",
+  /**상품리스트 페이지 URL
+   * @return "/product"
+   */
+  PRODUCT: "/product",
+  /**상품 상세 페이지 URL
+   * @return "/product/:productId"
+   */
+  PRODUCT_DETAIL: "/product/:productId",
+  /**카트 페이지 URL
+   * @return "/cart"
+   */
+  CART: "/cart",
+  /**주문 페이지 URL
+   * @return "/order"
+   */
+  ORDER: "/order",
+  /**주문 완료 페이지 URL
+   * @return "/order/complete"
+   */
+  ORDER_COMPLETE: "/order/complete",
 };
 
 /** SUPPORT: 후원 관련 URL 리스트*/
 export const SUPPORT = {
-	/**기부 페이지 URL
-	 * @return "/donate"
-	 */
-	DONATE: "/donate",
-	/**기부 신청 페이지 URL
-	 * @return "/donate/apply"
-	 */
-	APPLY: "/donate/apply",
-	/**봉사 게시판 페이지 URL
-	 * @return "/donate/volunteer"
-	 */
-	VOLUNTEER: "/donate/volunteer",
-	/**봉사 게시판 글작성 페이지 URL
-	 * @return "/donate/volunteer/write"
-	 */
-	VOLUNTEER_WRITE: "/donate/volunteer/write",
-	/**봉사 게시판 상세 페이지 URL
-	 * @return "/donate/volunteer/:id"
-	 */
-	VOLUNTEER_DETAIL: "/donate/volunteer/:id",
-	/**봉사 후기 게시판 페이지 URL
-	 * @return "/donate/volunteer/review"
-	 */
-	REVIEW: "/donate/volunteer/review",
-	/**봉사 후기 게시판 글작성 페이지 URL
-	 * @return "/donate/volunteer/review/write"
-	 */
-	REVIEW_WRITE: "/donate/volunteer/review/write",
-	/**봉사 후기 게시판 상세 페이지 URL
-	 * @return "/donate/volunteer/review:id"
-	 */
-	REVIEW_DETAIL: "/donate/volunteer/review:id",
+  /**기부 페이지 URL
+   * @return "/donate"
+   */
+  DONATE: "/donate",
+  /**기부 신청 페이지 URL
+   * @return "/donate/apply"
+   */
+  APPLY: "/donate/apply",
+  /**봉사 게시판 페이지 URL
+   * @return "/donate/volunteer/notice"
+   */
+  VOLUNTEER_NOTICE: "/donate/volunteer/notice",
+  /**봉사 게시판 글작성 페이지 URL
+   * @return "/donate/volunteer/notice/write"
+   */
+  VOLUNTEER_NOTICE_WRITE: "/donate/volunteer/notice/write",
+  /**봉사 게시판 상세 페이지 URL
+   * @return "/donate/volunteer/notice/:id"
+   */
+  VOLUNTEER_NOTICE_DETAIL: "/donate/volunteer/notice/:id",
+  /**봉사 후기 게시판 페이지 URL
+   * @return "/donate/volunteer/review"
+   */
+  VOLUNTEER_REVIEW: "/donate/volunteer/review",
+  /**봉사 후기 게시판 상세 페이지 URL
+   * @return "/donate/volunteer/review/write"
+   */
+  VOLUNTEER_REVIEW_WRITE: "/donate/volunteer/review/write",
+  /**봉사 후기 게시판 작성 페이지 URL
+   * @return "/donate/volunteer/review/:id"
+   */
+  VOLUNTEER_REVIEW_DETAIL: "/donate/volunteer/review/:id",
 };
 
 /** COMMUNITY: 커뮤니티 관련 URL 리스트*/
 export const COMMUNITY = {
-	/**목격 제보 게시판 페이지 URL
-	 * @return "/board/find"
-	 */
-	FIND: "/board/find",
-	/**자유게시판 페이지 URL
-	 * @return "/board/free"
-	 */
-	FREE: "/board/free",
-	/**매매장터 페이지 URL
-	 * @return "/board/flea"
-	 */
-	FLEA: "/board/flea",
-	/**실종 동물 게시판 페이지 URL
-	 * @return "board/missing"
-	 */
-	MISSING: "board/missing",
+  /**목격 제보 게시판 페이지 URL
+   * @return "/board/find"
+   */
+  FIND: "/board/find",
+  /**목격 제보 게시판 상세 페이지 URL
+   * @return "/board/find/:id"
+   */
+  FIND_DETAIL: "/board/find/:id",
+  /**목격 제보 게시판 작성 페이지 URL
+   * @return "/board/find/write"
+   */
+  FIND_WRITE: "/board/find/write",
+  /**자유게시판 페이지 URL
+   * @return "/board/free"
+   */
+  FREE: "/board/free",
+  /**자유게시판 상세 페이지 URL
+   * @return "/board/free/:id"
+   */
+  FREE_DETAIL: "/board/free/:id",
+  /**자유게시판 작성 페이지 URL
+   * @return "/board/free/write"
+   */
+  FREE_WRITE: "/board/free/write",
+  /**매매장터 페이지 URL
+   * @return "/board/flea"
+   */
+  FLEA: "/board/flea",
+  /**매매장터 상세 페이지 URL
+   * @return "/board/flea/:id"
+   */
+  FLEA_DETAIL: "/board/flea/:id",
+  /**매매장터 작성 페이지 URL
+   * @return "/board/flea/write"
+   */
+  FLEA_WRITE: "/board/flea/write",
+  /**실종 동물 게시판 페이지 URL
+   * @return "board/missing"
+   */
+  MISSING: "board/missing",
+  /**실종 동물 게시판 페이지 URL
+   * @return "board/missing"
+   */
+  MISSING_DETAIL: "board/missing/:id",
+  /**실종 동물 게시판 페이지 URL
+   * @return "board/missing/:id"
+   */
+  MISSING_WRITE: "board/missing/write",
 };
 
 /** ADMIN: 관리자 관련 URL 리스트*/
 export const ADMIN = {
-	/**회원 관리 페이지 URL
-	 * @return "/admin/member"
-	 */
-	MEMBER: "/admin/member",
-	/**입양 관리 페이지 URL
-	 * @return "/admin/adopt"
-	 */
-	ADOPT: "/admin/adopt",
-	/**사용자 대쉬보드 페이지 URL
-	 * @return "/admin/dashboard"
-	 */
-	DASHBOARD: "/admin/dashboard",
-	/**1:1 문의 관리 페이지 URL
-	 * @return "/admin/qna"
-	 */
-	QNA: "/admin/qna",
-	/**상품 관리 페이지 URL
-	 * @return "/admin/product"
-	 */
-	PRODUCT: "/admin/product",
-	/**상품 입력 페이지 URL
-	 * @return "/admin/product/form"
-	 */
-	PRODUCTFORM: "/admin/product/form",
-	/**주문 관리 페이지 URL
-	 * @return "/admin/order"
-	 */
-	ORDER: "/admin/order",
-	/**게시글 관리 페이지 URL
-	 * @return "/admin/board"
-	 */
-	BOARD: "/admin/board",
+  /**회원 관리 페이지 URL
+   * @return "/admin/member"
+   */
+  MEMBER: "/admin/member",
+  /**입양 관리 페이지 URL
+   * @return "/admin/adopt"
+   */
+  ADOPT: "/admin/adopt",
+  /**사용자 대쉬보드 페이지 URL
+   * @return "/admin/dashboard"
+   */
+  DASHBOARD: "/admin/dashboard",
+  /**1:1 문의 관리 페이지 URL
+   * @return "/admin/qna"
+   */
+  QNA: "/admin/qna",
+  /**상품 관리 페이지 URL
+   * @return "/admin/product"
+   */
+  PRODUCT: "/admin/product",
+  /**상품 입력 페이지 URL
+   * @return "/admin/product/form"
+   */
+  PRODUCTFORM: "/admin/product/form",
+  /**주문 관리 페이지 URL
+   * @return "/admin/order"
+   */
+  ORDER: "/admin/order",
+  /**게시글 관리 페이지 URL
+   * @return "/admin/board"
+   */
+  BOARD: "/admin/board",
 };
