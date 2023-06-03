acd9022131752055e7f84b646f37299ee474deff
dbtmdtt <dbtmdtt@gmail.com>
2023-06-03 13:59:36 +0900

    no message

--- src/pages/Community/Missing/MissingBoard.js
+++ src/pages/Community/Missing/MissingBoard.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { COMMUNITY } from '../../../constants/PageURL';
+import { COMMUNITY } from "../../../constants/PageURL";
 import styled from "styled-components";
 import { CustomTheme } from "../../../assets/Theme/CustomTheme";
-import {
-  ThemeProvider,
-  Grid,
-  Card,
-  Container,
-} from '@mui/material';
+import { ThemeProvider, Grid, Card, Container } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import SearchBar from "../../../components/common/SearchBar";
 import CustomButton from "../../Login/CustomButton";
@@ -38,7 +33,7 @@ const MissingBoard = () => {
   const getPageNum = () => {
     const maxLength = data.length;
     return setMaxPageNum(Math.ceil(maxLength / itemsPerPage));
-  }
+  };
 
   useEffect(() => {
     getPageNum();
@@ -50,9 +45,7 @@ const MissingBoard = () => {
     //게시글 목록 호출
     const fetchPost = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:8080/board/missing`
-        ); //게시글 데이터 호출
+        const response = await axios.get(`http://localhost:8080/board/missing`); //게시글 데이터 호출
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data : ", error);
@@ -72,13 +65,22 @@ const MissingBoard = () => {
     return <NotFound />; //존재하지 않는 번호를 넣었을 때 표시할 컴포넌트
   }
 
+  const formatDate = (dateString) => {
+    //날짜 변환함수
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}/${month}/${day}`;
+  };
+
   return (
     <ThemeProvider theme={CustomTheme}>
       <Section className="result">
         <MainContainer className="result-container">
           <ContainerBox>
             <Top>실종 동물 게시판</Top>
-            <Container sx={{ py: '30px' }} maxWidth="60vw">
+            <Container sx={{ py: "30px" }} maxWidth="60vw">
               <SearchContainer>
                 <SearchBar />
               </SearchContainer>
@@ -91,7 +93,10 @@ const MissingBoard = () => {
                   .map((card) => {
                     return (
                       <Grid item xs={10} sm={6} md={2} key={card.boardNum}>
-                        <Link to={COMMUNITY.MISSING_DETAIL(card.boardNum)} style={{ textDecoration: "none" }}>
+                        <Link
+                          to={COMMUNITY.MISSING_DETAIL(card.boardNum)}
+                          style={{ textDecoration: "none" }}
+                        >
                           <Card
                             key={card.boardNum}
                             sx={{
@@ -99,15 +104,14 @@ const MissingBoard = () => {
                               display: "flex",
                               flexDirection: "column",
                               border: "1px solid rgb(233, 236, 239)",
-                              boxShadow: "1px 1px 4px 0px rgb(233, 236, 239)"
+                              boxShadow: "1px 1px 4px 0px rgb(233, 236, 239)",
                             }}
                           >
                             <CardImage src={card.imgThumbnail} />
                             <div>
                               <CardTitle>{card.boardSubject}</CardTitle>
-                            </div>
-                            <div>
                               <CardWritter>{card.memberNickName}</CardWritter>
+                              <CardDate>{formatDate(card.boardDate)}</CardDate>
                               <CardCount>조회 {card.boardCount}</CardCount>
                             </div>
                           </Card>
@@ -116,13 +120,15 @@ const MissingBoard = () => {
                     );
                   })}
               </Grid>
-              {loggedIn === true ?
+              {loggedIn === true ? (
                 <Link to={COMMUNITY.MISSING_WRITE}>
                   <CustomButton label="글쓰기" value="글쓰기">
                     글쓰기
                   </CustomButton>
-                </Link> : <></>
-              }
+                </Link>
+              ) : (
+                <></>
+              )}
             </Container>
 
             <Pagination
@@ -133,25 +139,23 @@ const MissingBoard = () => {
               sx={{
                 display: "flex",
                 justifyContent: "center",
-                margin: '50px 0 0 0px'
+                margin: "50px 0 0 0px",
               }}
             />
-          </ContainerBox >
+          </ContainerBox>
         </MainContainer>
-      </Section >
+      </Section>
     </ThemeProvider>
-
   );
 };
 
 const Section = styled.section`
   background: #f8f9fa;
   padding: 30px 0 40px 0;
-`
+`;
 
 const MainContainer = styled.div`
-  width: 60vw;
-  // width: 1150px;
+  width: 1008px;
   max-width: 1150px;
   min-width: 790px;
   border-radius: 8px;
@@ -161,14 +165,14 @@ const MainContainer = styled.div`
   border-image: initial;
   margin: 0px auto 20px;
   background: rgb(255, 255, 255);
-`
+`;
 
 const Top = styled.h1`
-      font-size: 2rem;
-      font-weight: bold;
-      text-align: center;
-      margin-bottom: 2rem;
-      `;
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
 
 const SearchContainer = styled.div`
   float: right;
@@ -177,8 +181,9 @@ const SearchContainer = styled.div`
 `;
 
 const CardImage = styled.img`
+  // fullWidth;
   width: auto;
-  height: 250px;
+  height: 196px;
   object-fit: cover;
   background-size: cover;
   background-repeat: no-repeat;
@@ -186,35 +191,41 @@ const CardImage = styled.img`
   border: 1px solid #ccc;
   border-radius: 0.5rem;
   margin: 0.5rem;
-  `;
+`;
 
 const CardTitle = styled.p`
-      font-weight: bold;
-      font-size: 1.1rem;
-      margin: 0.3em 0.5em 0em 0.5em;
-      line-height: 1.2em;
-      height: 2.4em;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
-      `;
+  font-weight: bold;
+  font-size: 1.1rem;
+  margin: 0.3em 0.5em 0em 0.5em;
+  line-height: 1.2em;
+  height: 2.4em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
 
 const CardWritter = styled.p`
-      font-size: 14px;
-      color: #888;
-      margin-left: 10px
-      `;
+  font-size: 14px;
+  color: #888;
+  margin-left: 10px;
+`;
 
 const CardCount = styled.p`
-      font-size: 14px;
-      color: #888;
-      float: right;
-      margin-right: 10px;
-      `
+  font-size: 14px;
+  color: #888;
+  float: right;
+  margin-right: 10px;
+`;
 
-const ContainerBox = styled.div`
-      `
+const CardDate = styled.p`
+  font-size: 14px;
+  color: #888;
+  float: left;
+  margin-left: 10px;
+`;
+
+const ContainerBox = styled.div``;
 
 export default MissingBoard;
