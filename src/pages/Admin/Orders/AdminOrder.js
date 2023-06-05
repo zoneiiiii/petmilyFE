f67345a51d5057e86b077e7978655e221c0cff0c
sam4877 <8ree_@naver.com>
2023-06-05 09:48:51 +0900

    관리자 주문/배송페이지 추가

--- src/pages/Admin/Orders/AdminOrder.js
+++ src/pages/Admin/Orders/AdminOrder.js
@@ -1,4 +1,3 @@
-import styleds from "styled-components";
 import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -6,16 +5,16 @@ import TableCell from "@mui/material/TableCell";
 import Grid from "@mui/material/Grid";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { tableCellClasses } from "@mui/material/TableCell";
 import { Container, Pagination, Paper } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
-import { Link } from "react-router-dom";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { Checkbox, Button } from "@mui/material";
-import { useState } from "react";
+import Select from "@mui/material/Select";
+import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
 
 const theme = createTheme({
   palette: {
@@ -26,163 +25,58 @@ const theme = createTheme({
   },
 });
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: theme.palette.common.black,
-    fontWeight: "bold",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  "td,th": {
-    border: "1px solid lightgray",
-  },
-}));
-
 const AdminOrder = () => {
-  const [orders, setOrders] = useState([
-    {
-      num: 7,
-      date: "2023-04-26",
-      delivery: "shipping",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 2,
-      buyerID: "test07",
-      payment: "무통장입금",
-      paymentCheck: "X",
-      paymentAmount: 20000,
-      checked: false,
-    },
-    {
-      num: 6,
-      date: "2023-04-25",
-      delivery: "shipping",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 1,
-      buyerID: "test06",
-      payment: "신용카드",
-      paymentCheck: "O",
-      paymentAmount: 10000,
-      checked: false,
-    },
-    {
-      num: 5,
-      date: "2023-04-25",
-      delivery: "complete",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 2,
-      buyerID: "test05",
-      payment: "계좌이체",
-      paymentCheck: "O",
-      paymentAmount: 20000,
-      checked: false,
-    },
-    {
-      num: 4,
-      date: "2023-04-24",
-      delivery: "complete",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 3,
-      buyerID: "test04",
-      payment: "계좌이체",
-      paymentCheck: "O",
-      paymentAmount: 30000,
-      checked: false,
-    },
-    {
-      num: 3,
-      date: "2023-04-22",
-      delivery: "complete",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 1,
-      buyerID: "test03",
-      payment: "계좌이체",
-      paymentCheck: "O",
-      paymentAmount: 10000,
-      checked: false,
-    },
-    {
-      num: 2,
-      date: "2023-04-20",
-      delivery: "complete",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 1,
-      buyerID: "test02",
-      payment: "계좌이체",
-      paymentCheck: "O",
-      paymentAmount: 10000,
-      checked: false,
-    },
-    {
-      num: 1,
-      date: "2023-04-18",
-      delivery: "complete",
-      PRcode: "000010",
-      PRname: "유기농 강아지 사료",
-      count: 1,
-      buyerID: "test01",
-      payment: "계좌이체",
-      paymentCheck: "O",
-      paymentAmount: 10000,
-      checked: false,
-    },
-  ]);
-
-  const [selected, setSelected] = useState([]);
+  const [orders, setOrders] = useState([]);
   const [page, setPage] = React.useState(1);
   const rowsPerPage = 5;
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-  const [delivery, setDelivery] = React.useState("");
 
-  const handleChange = (event) => {
-    setDelivery(event.target.value);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/a/order/orders")
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleDeleteOrder = async (orderNum) => {
+    try {
+      await axios.post(`/a/order/orders/${orderNum}`);
+      const response = await axios.get("/a/order/orders");
+      setOrders(response.data);
+    } catch (error) {
+      console.error("주문 삭제 실패:", error);
+    }
   };
 
-  //전체선택 기능
-  const handleToggleAll = () => {
-    const allChecked = orders.every((order) => order.checked);
-    const updatedOrders = orders.map((order) => ({
-      ...order,
-      checked: !allChecked,
-    }));
-    setOrders(updatedOrders);
+  const handleDeleteButtonClick = (orderNum) => {
+    if (window.confirm("주문을 삭제하시겠습니까?")) {
+      handleDeleteOrder(orderNum);
+      alert("주문 삭제가 완료되었습니다.");
+    }
   };
 
-  const allChecked = orders.every((order) => order.checked);
-  const indeterminate = !allChecked && orders.some((order) => order.checked);
-  //체크박스 기능
-  const handleToggle = (orderId) => {
-    const updatedOrders = orders.map((order) => {
-      if (order.num === orderId) {
-        return {
-          ...order,
-          checked: !order.checked,
-        };
-      }
-      return order;
-    });
-    setOrders(updatedOrders);
-  };
-  //체크된 상품 삭제(백엔드 하면서 수정 필요)
-  const handleDeleteSelected = (e) => {
-    e.preventDefault(); // 폼의 기본 동작 방지
-    setOrders(orders.filter((order) => !selected.includes(order.id)));
-    setSelected([]);
+  const handleRoleChange = (orderNum, newState) => {
+    if (window.confirm("주문상태를 수정하시겠습니까?")) {
+      axios
+        .put(`/a/order/updateState/${orderNum}`, { orderState: newState })
+        .then((response) => {
+          window.alert("주문상태가 수정 되었습니다");
+          return axios.get("/a/order/orders", {});
+        })
+        .then((response) => {
+          setOrders(response.data);
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+          window.alert("수정 실패");
+        });
+    }
   };
 
   return (
@@ -190,14 +84,6 @@ const AdminOrder = () => {
       <ThemeProvider theme={theme}>
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
           <Paper sx={{ p: 1, mb: 2 }}>
-            <MyPageStyle>
-              <div className="navTitle">
-                <h5>주문 배송 관리</h5>
-              </div>
-              <Button className="prodDelete" onClick={handleDeleteSelected}>
-                선택 주문 삭제
-              </Button>
-            </MyPageStyle>
             <Grid style={{ width: 1135 }}>
               <Table
                 sx={{
@@ -208,48 +94,32 @@ const AdminOrder = () => {
                 overflow="hidden"
               >
                 <TableHead>
-                  <StyledTableRow>
-                    <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                      <Checkbox
-                        edge="start"
-                        checked={allChecked}
-                        indeterminate={indeterminate}
-                        onClick={handleToggleAll}
-                        tabIndex={-1}
-                      />
-                      전체선택
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                  <TableRow>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       주문번호
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 20 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       주문일자
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       배송상태
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       상품코드
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
-                      상품명
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       구매수량
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       구매자ID
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
-                      결제방식
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
-                      결제여부
-                    </StyledTableCell>
-                    <StyledTableCell align="center" sx={{ minWidth: 10 }}>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
                       총 결제금액(원)
-                    </StyledTableCell>
-                  </StyledTableRow>
+                    </TableCell>
+                    <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                      관리
+                    </TableCell>
+                  </TableRow>
                 </TableHead>
                 <TableBody>
                   {orders
@@ -257,57 +127,64 @@ const AdminOrder = () => {
                       (page - 1) * rowsPerPage,
                       (page - 1) * rowsPerPage + rowsPerPage
                     )
-                    .map((order) => (
-                      <StyledTableRow key={order.num}>
-                        <StyledTableCell align="center" sx={{ minWidth: 10 }}>
-                          <Checkbox
-                            edge="start"
-                            checked={order.checked}
-                            onClick={() => handleToggle(order.num)}
-                            tabIndex={-1}
-                          />
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 10 }}>
-                          {order.num}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.date}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          <FormControl sx={{ m: 1, minWidth: 120 }}>
-                            <Select
-                              defaultValue={order.delivery}
-                              onChange={handleChange}
-                              displayEmpty
+                    .map((order) => {
+                      return (
+                        <TableRow key={order.orderNum}>
+                          <TableCell align="center">{order.orderNum}</TableCell>
+                          <TableCell align="center">
+                            {dayjs(order.orderDate).format("YY/MM/DD HH:mm")}
+                          </TableCell>
+                          <TableCell align="center">
+                            <FormControl sx={{ m: 1, minWidth: 120 }}>
+                              <Select
+                                sx={{
+                                  fontSize: "13px",
+                                  width: "100px",
+                                  height: "40px",
+                                }}
+                                value={order.orderState}
+                                onChange={(e) =>
+                                  handleRoleChange(
+                                    order.orderNum,
+                                    e.target.value
+                                  )
+                                }
+                                displayEmpty
+                              >
+                                <MenuItem
+                                  sx={{ fontSize: "13px" }}
+                                  value="complete"
+                                >
+                                  배송완료
+                                </MenuItem>
+                                <MenuItem
+                                  sx={{ fontSize: "13px" }}
+                                  value="shipping"
+                                >
+                                  배송중
+                                </MenuItem>
+                              </Select>
+                            </FormControl>
+                          </TableCell>
+                          <TableCell align="center">{order.boardNum}</TableCell>
+                          <TableCell align="center">{order.quantity}</TableCell>
+                          <TableCell align="center">
+                            {order.memberNum}
+                          </TableCell>
+                          <TableCell align="center">{order.cost}</TableCell>
+                          <TableCell align="center">
+                            <Button
+                              className="prodDelete"
+                              onClick={() =>
+                                handleDeleteButtonClick(order.orderNum)
+                              }
                             >
-                              <MenuItem value="complete">배송완료</MenuItem>
-                              <MenuItem value="shipping">배송중</MenuItem>
-                            </Select>
-                          </FormControl>
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.PRcode}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.PRname}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.count}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.buyerID}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.payment}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.paymentCheck}
-                        </StyledTableCell>
-                        <StyledTableCell align="center" sx={{ minWidth: 30 }}>
-                          {order.paymentAmount}
-                        </StyledTableCell>
-                      </StyledTableRow>
-                    ))}
+                              삭제
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
                 </TableBody>
               </Table>
 
@@ -331,30 +208,5 @@ const AdminOrder = () => {
     </>
   );
 };
-
-const MyPageStyle = styleds.div`
-  .navTitle {
-    border: 1px solid #fbd385;
-    width: 200px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 30px 0 0 0;
-  }
-  .prodDelete {
-    float: left;
-    background-color: #fbd385;
-    color: white;
-    margin: 20px 0 20px 0;
-    &:hover {
-      background-color: #facc73;
-    }
-    &:focus {
-      background-color: #facc73;
-    }
-    font-weight: bold;
-  }
-`;
 
 export default AdminOrder;
