5bce3b9cc97469cd266d0b1b48bcb33dd17324fd
Taemin Eom <umtaemin1125@naver.com>
2023-06-07 09:37:42 +0900

    feat:상품판매 누적금액 백엔드 데이터 연동

--- src/pages/Admin/Dashboard/Deposits.js
+++ src/pages/Admin/Dashboard/Deposits.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Title from "./Title";
 import dayjs from "dayjs";
@@ -7,15 +6,30 @@ import "dayjs/locale/ko";
 import axios from "axios";
 
 export default function Deposits() {
+  const [totalCost, setTotalCost] = useState(0);
   const formatCurrency = (number) => {
     return number.toLocaleString("ko-KR", { currency: "KRW" }) + "원";
   };
+
+  const fetchTotalCost = async () => {
+    try {
+      const response = await axios.get("/order/allOrdersCost");
+      const totalCost = response.data;
+      setTotalCost(totalCost);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTotalCost();
+  }, []);
 
   return (
     <React.Fragment>
       <Title>누적 판매금</Title>
       <Typography component="p" variant="h4">
-        {formatCurrency(447220)}
+        {formatCurrency(totalCost)}
       </Typography>
       <Typography
         color="text.secondary"
